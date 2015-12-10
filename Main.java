9a8dee1a043f167a338468f9329a3990b4e8fb3e
garai <garai@crooz.co.jp>
2015-12-10 17:07:52 +0900

    intial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+﻿ public class Main{
+  public static void main(String[]args){
+	long worldwide ; worldwide=700000L;
+	int salaly ; salaly=250000;
+	short age ; age=18;
+	byte grasses ; grasses=2;
+	System.out.println ("人口"+worldwide);
+	System.out.println ("年齢"+age);
+	System.out.println ("給与"+salaly);
+	System.out.println ("メガネのレンズ"+grasses);
+  } 
+ }
---
+++ MyStudyA.java
@@ -0,0 +1,14 @@
+﻿  /**
+   java　テキスト2章問題１
+    */
+ 
+ 
+ public class MyStudyA {
+  public static void main(String[]args){
+	int x = 5;
+	int y = 10;
+	int z = x + y;
+	String ans = "x+yは" + z;
+	System.out.println (ans);
+ }
+  }
