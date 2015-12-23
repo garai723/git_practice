e811cd82e3bfb1ca4ebdf3d0b63b2c65c3342f71
garai <garai@crooz.co.jp>
2015-12-23 19:05:53 +0900

    review

---
+++ review/ReviewSwitch.java
@@ -0,0 +1,27 @@
+
+public class ReviewSwitch {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("what is your favorite number?");
+		int a = new java.util.Scanner(System.in).nextInt();
+		switch(a){
+		case 1:
+			System.out.println("you r simple");
+			break;
+
+		case 2:
+			System.out.println("you r normal");
+			break;
+
+		case 3:
+			System.out.println("you r noisy ");
+			break;
+
+		default:
+			System.out.println("oh my god");
+
+		}
+	}
+
+}
