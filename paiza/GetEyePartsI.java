f3e0f1ac455eaa06c46d8f2490b0304403fef383
garai <garai@crooz.co.jp>
2015-12-23 20:18:01 +0900

    initial commit

---
+++ paiza/GetEyePartsI.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GetEyePartsI {
+    public static void main(String[] args) throws Exception {
+            // 自分の得意な言語で
+	            // Let's チャレンジ！！
+		           
+			           int a = new java.util.Scanner(System.in).nextInt();
+				           for(int i = 0; i<a; i++){
+					           System.out.print("Ann");
+						           }
+							       }
+							       }
+
